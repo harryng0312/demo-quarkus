bd09e274c8a9dc2d8908c592b30402b84031daad
harryng <harry@videabiz.com>
2022-04-04 10:51:26 +0700

    - build with G1GC

--- src/test/java/org/harryng/demo/quarkus/ExampleResourceTest.java
+++ src/test/java/org/harryng/demo/quarkus/ExampleResourceTest.java
@@ -1,7 +1,12 @@
 package org.harryng.demo.quarkus;
 
+import io.quarkus.logging.Log;
 import io.quarkus.test.junit.QuarkusTest;
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
@@ -16,6 +21,43 @@ public class ExampleResourceTest {
                 .then()
                 .statusCode(200)
                 .body(is("Hello RESTEasy"));
+        Log.info("test hello");
     }
 
+    @Test
+    public void testStringUni() {
+        Log.info("test uni");
+        Uni.createFrom().item("string value")
+            .onItem().transform(val -> {
+                Log.info(val);
+                return val + " after 1";
+            })
+            .onItem().transform(val -> {
+                Log.info(val);
+                return val;
+            })
+            .subscribe().with(val -> Log.info(val));
+    }
+
+    @Test
+    public void testStringMulti() {
+        Log.info("test multi");
+        var values = new String[]{"0", "1", "3", "4", "5"};
+        Multi.createFrom()
+            .emitter(emitter -> {
+                Arrays.stream(values).forEach(itm -> {
+                    try {
+                        Thread.sleep(2 * 1_000);
+                        emitter.emit(itm);
+//                            Log.info("emit: " + itm);
+                    } catch (InterruptedException e) {
+                        Log.error("", e);
+                    }
+                });
+            })
+            .subscribe().with(
+                    item -> Log.info("Item val:" + item),
+                    failure -> Log.error("Failed with " + failure),
+                    () -> Log.info("Completed"));
+    }
 }
