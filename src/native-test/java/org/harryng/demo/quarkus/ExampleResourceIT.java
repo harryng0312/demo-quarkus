6d57fcc2be14c4bd8e0f0751d035cf5c0d4eb8c6
harryng <harry@videabiz.com>
2022-04-04 11:29:49 +0700

    - build with G1GC

--- src/native-test/java/org/harryng/demo/quarkus/ExampleResourceIT.java
+++ src/native-test/java/org/harryng/demo/quarkus/ExampleResourceIT.java
@@ -1,9 +1,27 @@
 package org.harryng.demo.quarkus;
 
+import io.quarkus.logging.Log;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import io.smallrye.mutiny.Uni;
+import org.junit.jupiter.api.Test;
 
 @QuarkusIntegrationTest
 public class ExampleResourceIT extends ExampleResourceTest {
 
     // Execute the same tests but in native mode.
+    @Test
+    public void testStringUni() {
+        Log.info("test uni in native");
+        Uni.createFrom().item("string value")
+                .onItem().transform(val -> {
+                    Log.info(val);
+                    return val + " after 1";
+                })
+                .onItem().transform(val -> {
+                    Log.info(val);
+                    return val;
+                })
+                .subscribe().with(item -> Log.info(item));
+    }
+
 }
