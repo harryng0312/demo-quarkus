7348e400963cbf612b727c11ddcccecec885c10b
harryng <harry@videabiz.com>
2022-04-07 11:53:30 +0700

    - basic marshalling

--- src/main/java/org/harryng/demo/quarkus/controller/UserController.java
+++ src/main/java/org/harryng/demo/quarkus/controller/UserController.java
@@ -1,16 +1,15 @@
 package org.harryng.demo.quarkus.controller;
 
 import io.quarkus.logging.Log;
+import io.smallrye.common.annotation.NonBlocking;
 import io.vertx.core.http.HttpServerRequest;
+import org.harryng.demo.quarkus.user.entity.UserImpl;
 import org.harryng.demo.quarkus.user.service.UserService;
 import org.harryng.demo.quarkus.util.SessionHolder;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.Collections;
 
@@ -27,6 +26,7 @@ public class UserController {
     @GET
     @Path("/get-username-sync")
     @Produces({MediaType.TEXT_PLAIN})
+//    @NonBlocking
     public String getUsernameSync(@QueryParam("id") long id){
         String rs = "";
         try {
@@ -39,6 +39,25 @@ public class UserController {
                 strBuilder.append(rs);
             }
             rs = strBuilder.toString();
+        } catch (Exception e) {
+            Log.error("", e);
+        }
+        return rs;
+    }
+
+    @GET
+    @Path("/get-user-by-id")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+//    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+//    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    public UserImpl getUserById(@QueryParam("id") long id){
+        UserImpl rs = null;
+        try {
+            var opt = userService.getById(SessionHolder.createAnonymousSession(), id, Collections.emptyMap());
+            if(opt.isPresent()){
+                rs = opt.get();
+            }
         } catch (Exception e) {
             Log.error("", e);
         }
--- src/main/java/org/harryng/demo/quarkus/user/entity/UserImpl.java
+++ src/main/java/org/harryng/demo/quarkus/user/entity/UserImpl.java
@@ -1,8 +1,14 @@
 package org.harryng.demo.quarkus.user.entity;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+//@XmlRootElement
+//@XmlAccessorType(XmlAccessType.PROPERTY)
 public class UserImpl extends UserModel {
 
     public UserImpl(){super();}
