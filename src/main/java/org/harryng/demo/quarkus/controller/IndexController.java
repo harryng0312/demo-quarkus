80fdc6507c13dcc40a5b7f5f3b8774df6ccf53c1
harrynguyen0312 <Hiepxie1987>
2022-04-25 10:12:12 +0700

    - vertx

--- src/main/java/org/harryng/demo/quarkus/controller/IndexController.java
+++ src/main/java/org/harryng/demo/quarkus/controller/IndexController.java
@@ -1,14 +1,12 @@
 package org.harryng.demo.quarkus.controller;
 
-import io.quarkus.logging.Log;
 import io.quarkus.qute.Location;
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
 import io.smallrye.mutiny.Uni;
 import io.smallrye.mutiny.unchecked.Unchecked;
-
-import java.time.Duration;
-import java.time.LocalDateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -18,9 +16,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 @Path("/")
 public class IndexController {
@@ -50,103 +47,103 @@ public class IndexController {
     @GET
     @Path("/test-duration-nonblock")
     @Produces(MediaType.APPLICATION_JSON)
-    public Uni<Response> testDuration(){
+    public Uni<Response> testDuration() {
         // var res = Response.status(Status.INTERNAL_SERVER_ERROR).build();
         logger.info("test uni");
         return Uni.createFrom().item("string value")
-            .map(val -> {
-                var startTime = LocalDateTime.now();
-                logger.info("START AT: " + startTime);
-                logger.info(val);
-                return val + " after 1";
-            })
-            .map(Unchecked.function(val -> {
-                var startTime = LocalDateTime.now();
-                logger.info("start1 at: " + startTime);
-                Thread.sleep(2 * 1_000);
-                var finishedTime = LocalDateTime.now();
-                logger.info("duration time 1: " + (Duration.between(startTime, finishedTime)));
-                return val;
-            }))
-            .map(Unchecked.function(val -> {
-                var startTime = LocalDateTime.now();
-                logger.info("start2 at: " + startTime);
-                Thread.sleep(3 * 1_000);
-                var finishedTime = LocalDateTime.now();
-                logger.info("duration time 2: " + (Duration.between(startTime, finishedTime)));
-                return val;
-            }))
-            // .onItem().delayIt().by(Duration.ofMillis(100))
-            // .map(Unchecked.function(val -> {
-            //     var finishedTime = LocalDateTime.now();
-            //     logger.info("finished at:" + finishedTime);
-            //     return val;
-            // }))
-            .map(Unchecked.function(val -> {
-                var nowTime = LocalDateTime.now();
-                logger.info("FINISHED AT: " + nowTime);
-                logger.info("sub: " + val);
-                return Response.ok("OK").build();
-            }))
-            .onFailure().transform(Unchecked.function(err -> {
-                logger.error("sub err: ", err);
-                return new WebApplicationException(err);
-            }));
-            // .subscribe().with(
-            //     val -> {
-            //         var nowTime = LocalDateTime.now();
-            //         logger.info("FINISHED AT: " + nowTime);
-            //         logger.info("sub: " + val);
-            //     },
-            //     err -> logger.error("sub err: ", err)
-            // );
+                .map(val -> {
+                    var startTime = LocalDateTime.now();
+                    logger.info("START AT: " + startTime);
+                    logger.info(val);
+                    return val + " after 1";
+                })
+                .map(Unchecked.function(val -> {
+                    var startTime = LocalDateTime.now();
+                    logger.info("start1 at: " + startTime);
+                    Thread.sleep(2 * 1_000);
+                    var finishedTime = LocalDateTime.now();
+                    logger.info("duration time 1: " + (Duration.between(startTime, finishedTime)));
+                    return val;
+                }))
+                .map(Unchecked.function(val -> {
+                    var startTime = LocalDateTime.now();
+                    logger.info("start2 at: " + startTime);
+                    Thread.sleep(3 * 1_000);
+                    var finishedTime = LocalDateTime.now();
+                    logger.info("duration time 2: " + (Duration.between(startTime, finishedTime)));
+                    return val;
+                }))
+                // .onItem().delayIt().by(Duration.ofMillis(100))
+                // .map(Unchecked.function(val -> {
+                //     var finishedTime = LocalDateTime.now();
+                //     logger.info("finished at:" + finishedTime);
+                //     return val;
+                // }))
+                .map(Unchecked.function(val -> {
+                    var nowTime = LocalDateTime.now();
+                    logger.info("FINISHED AT: " + nowTime);
+                    logger.info("sub: " + val);
+                    return Response.ok("OK").build();
+                }))
+                .onFailure().transform(Unchecked.function(err -> {
+                    logger.error("sub err: ", err);
+                    return new WebApplicationException(err);
+                }));
+        // .subscribe().with(
+        //     val -> {
+        //         var nowTime = LocalDateTime.now();
+        //         logger.info("FINISHED AT: " + nowTime);
+        //         logger.info("sub: " + val);
+        //     },
+        //     err -> logger.error("sub err: ", err)
+        // );
         // return Uni.createFrom().item(res);
     }
 
     @GET
     @Path("/test-duration2-nonblock")
     @Produces(MediaType.APPLICATION_JSON)
-    public Uni<Response> testDuration2(){
+    public Uni<Response> testDuration2() {
         logger.info("test uni2");
         return Uni.createFrom().item("string value")
-        .map(val -> {
-            var startTime = LocalDateTime.now();
-            logger.info("START AT: " + startTime);
-            logger.info(val);
-            return val + " after 1";
-        })
-        .map(Unchecked.function(val -> {
-            var startTime = LocalDateTime.now();
-            logger.info("start1 at: " + startTime);
-            return val;
-        }))
-        .onItem().delayIt().by(Duration.ofSeconds(2))
-        .map(val -> {
-            var finishedTime = LocalDateTime.now();
-            logger.info("finished1 at: " + finishedTime);
-            return val;
-        })
-        .map(Unchecked.function(val -> {
-            var startTime = LocalDateTime.now();
-            logger.info("start2 at: " + startTime);
-            return val;
-        }))
-        .onItem().delayIt().by(Duration.ofSeconds(3))
-        .map(val -> {
-            var finishedTime = LocalDateTime.now();
-            logger.info("finished2 at: " + finishedTime);
-            return val;
-        })
-        .map(Unchecked.function(val -> {
-            var nowTime = LocalDateTime.now();
-            logger.info("FINISHED AT: " + nowTime);
-            logger.info("sub: " + val);
-            return Response.ok("OK").build();
-        }))
-        .onFailure().transform(Unchecked.function(err -> {
-            logger.error("sub err: ", err);
-            return new WebApplicationException(err);
-        }));
+                .map(val -> {
+                    var startTime = LocalDateTime.now();
+                    logger.info("START AT: " + startTime);
+                    logger.info(val);
+                    return val + " after 1";
+                })
+                .map(Unchecked.function(val -> {
+                    var startTime = LocalDateTime.now();
+                    logger.info("start1 at: " + startTime);
+                    return val;
+                }))
+                .onItem().delayIt().by(Duration.ofSeconds(2))
+                .map(val -> {
+                    var finishedTime = LocalDateTime.now();
+                    logger.info("finished1 at: " + finishedTime);
+                    return val;
+                })
+                .map(Unchecked.function(val -> {
+                    var startTime = LocalDateTime.now();
+                    logger.info("start2 at: " + startTime);
+                    return val;
+                }))
+                .onItem().delayIt().by(Duration.ofSeconds(3))
+                .map(val -> {
+                    var finishedTime = LocalDateTime.now();
+                    logger.info("finished2 at: " + finishedTime);
+                    return val;
+                })
+                .map(Unchecked.function(val -> {
+                    var nowTime = LocalDateTime.now();
+                    logger.info("FINISHED AT: " + nowTime);
+                    logger.info("sub: " + val);
+                    return Response.ok("OK").build();
+                }))
+                .onFailure().transform(Unchecked.function(err -> {
+                    logger.error("sub err: ", err);
+                    return new WebApplicationException(err);
+                }));
     }
 
 }
