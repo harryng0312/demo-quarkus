eb87c8a2c0b3a2d6e2481994f0bb67b1a9f19fec
harryng0312 <little.ruan@gmail.com>
2022-04-25 16:50:53 +0700

    - add vuejs

--- gui/src/ts/Authentication.ts
+++ gui/src/ts/Authentication.ts
@@ -1,7 +1,7 @@
 import {getTokenStore} from "@/stores/counter";
 import type {RouteLocationNormalized} from "vue-router";
 
-function isAuthenticated(to: RouteLocationNormalized, from: RouteLocationNormalized) {
+function isAuthenticated(to: RouteLocationNormalized, from: RouteLocationNormalized): boolean {
     let token = getTokenStore().token;
     console.log(`from:[${from.path}] to:[${to.path}]`);
     return token !== "";
